ca17a190413cc964bd8f28847e469af23197a340
Егор Краснов <yegor.krasnov.ua@itlab-studio.com>
2019-10-22 12:46:42 +0300

    bundle

--- src/Exchange/DependencyInjection/ExchangeExtension.php
+++ src/Exchange/DependencyInjection/ExchangeExtension.php
@@ -15,20 +15,13 @@ final class ExchangeExtension extends Extension
     /**
      * Loads a specific configuration.
      *
-     * @throws \InvalidArgumentException When provided tag is not defined in this extension
+     * @param array $configs
+     * @param ContainerBuilder $container
      * @throws \Exception
      */
     public function load(array $configs, ContainerBuilder $container)
     {
-        $processor = new Processor();
-
         $loader = new XmlFileLoader($container, new FileLocator(__DIR__.'/../Resources/config'));
         $loader->load('exchange.xml');
-
-        $configuration = new Configuration();
-        $config = $processor->processConfiguration($configuration, $configs);
-        $paginatorDef = $container->getDefinition('exchange');
-
-        $paginatorDef->setLazy(true);
     }
 }
