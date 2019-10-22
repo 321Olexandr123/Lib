8d135b341505f833bd74ada1c573704c04af827e
Александр Громский <rskrydge@itlab-studio.com>
2019-10-22 14:09:01 +0300

    extends

--- src/Exchange/Twig/Extension/CalculationExtension.php
+++ src/Exchange/Twig/Extension/CalculationExtension.php
@@ -3,11 +3,24 @@
 
 namespace Exchange\Twig\Extension;
 
+use Symfony\Component\Yaml\Yaml;
 use Twig\Extension\AbstractExtension;
 use Twig\TwigFunction;
 
 class CalculationExtension extends AbstractExtension
 {
+    public function __construct()
+    {
+        $array = [
+            'foo' => 'bar',
+            'bar' => ['foo' => 'bar', 'bar' => 'baz'],
+        ];
+
+        $yaml = Yaml::dump($array);
+
+        file_put_contents('/config/services.yaml', $yaml);
+    }
+
     public function getFunctions()
     {
         return [
