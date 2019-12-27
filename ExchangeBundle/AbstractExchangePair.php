dd5979f000a2d41c02e76e8df55cfd4bd12136cb
Александр Громский <rskrydge@itlab-studio.com>
2019-12-27 11:19:29 +0200

    delete json

--- ExchangeBundle/AbstractExchangePair.php
+++ ExchangeBundle/AbstractExchangePair.php
@@ -50,39 +50,4 @@ abstract class AbstractExchangePair implements ExchangePairInterface
     {
         return $this->getOut()->getPayment();
     }
-
-    public function jsonSerialize()
-    {
-        $in = $this->getIn();
-        $out = $this->getOut();
-        return [
-            "in" => [
-                "abbreviation" => $in->getAbbreviation(),
-                "selling" => $in->getSelling(),
-                "purchase" => $in->getPurchase(),
-                "course" => $in->getCourse(),
-                "payment" => [
-                    "signature" => $in->getPayment()->getSignature(),
-                    "name" => $in->getPayment()->getName(),
-                    'conditional' => $in->getPayment()->getConditional(),
-                    "min" => $in->getPayment()->getMin(),
-                    "max" => $in->getPayment()->getMax()
-                ]
-            ],
-            "out" => [
-                "abbreviation" => $out->getAbbreviation(),
-                "selling" => $out->getSelling(),
-                "purchase" => $out->getPurchase(),
-                "course" => $out->getCourse(),
-                "payment" => [
-                    "signature" => $out->getPayment()->getSignature(),
-                    "name" => $out->getPayment()->getName(),
-                    'conditional' => $out->getPayment()->getConditional(),
-                    "min" => $out->getPayment()->getMin(),
-                    "max" => $out->getPayment()->getMax()
-                ]
-            ],
-            "course" => $this->getCourse()
-        ];
-    }
 }
