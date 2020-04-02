c5390b083d70c00e855af869cd09207d8635f014
Александр Громский <rskrydge@itlab-studio.com>
2020-04-02 14:42:57 +0300

    add min and max for payment

--- Utils/PaymentSystemInterface.php
+++ Utils/PaymentSystemInterface.php
@@ -10,4 +10,8 @@ interface PaymentSystemInterface
      * @return string
      */
     public function getName(): ?string;
+
+    public function getMin(): ?float;
+
+    public function getMax(): ?float;
 }
